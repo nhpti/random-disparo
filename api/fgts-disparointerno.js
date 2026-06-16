03d76268fd65d3fefd3b756ef378e251507cb441
NHP <ti@nhpromotora.com>
2026-06-16 17:17:07 -0300

    Altera mensagem di1: Olá Novo Horizonte! Quero saber mais sobre o Crédito do Trabalhador!

--- api/fgts-disparointerno.js
+++ api/fgts-disparointerno.js
@@ -11,7 +11,7 @@ const { getNextNumero } = require('../lib/round-robin');
 const TABELA_NUMEROS = 'numeros';
 
 // Mensagem pré-preenchida que aparece no WhatsApp
-const MENSAGEM = '(di1) Olá Novo Horizonte! Quero sacar meu FGTS e receber agora!';
+const MENSAGEM = '(di1) Olá Novo Horizonte! Quero saber mais sobre o Crédito do Trabalhador!';
 
 module.exports = async function handler(req, res) {
   if (req.method !== 'GET') {
